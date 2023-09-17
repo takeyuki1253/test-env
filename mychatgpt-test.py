4251d296d8b65d407542c1fa9a5c7cd15d6de64a
takeumi <39755025+takeyuki1253@users.noreply.github.com>
2023-09-17 16:41:37 +0900

    Update mychatgpt-test.py

--- mychatgpt-test.py
+++ mychatgpt-test.py
@@ -1,19 +1,24 @@
 import streamlit as st
-from langchain.callbacks import StreamlitCallbackHandler
+from streamlit_chat import message
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
     SystemMessage,
     HumanMessage,
     AIMessage
 )
+from langchain.callbacks import get_openai_callback
+
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 
 def init_page():
     st.set_page_config(
-        page_title="My Great ChatGPT",
+        page_title="Website Summarizer",
         page_icon="🤗"
     )
-    st.header("My Great ChatGPT 🤗")
+    st.header("Website Summarizer 🤗")
     st.sidebar.title("Options")
 
 
@@ -33,11 +38,56 @@ def select_model():
     else:
         model_name = "gpt-4"
 
-    # サイドバーにスライダーを追加し、temperatureを0から2までの範囲で選択可能にする
-    # 初期値は0.0、刻み幅は0.1とする
-    temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=0.0, step=0.01)
+    return ChatOpenAI(temperature=0, model_name=model_name)
 
-    return ChatOpenAI(temperature=temperature, model_name=model_name, streaming=True)
+
+def get_url_input():
+    url = st.text_input("URL: ", key="input")
+    return url
+
+
+def validate_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+
+def get_content(url):
+    try:
+        with st.spinner("Fetching Content ..."):
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            # fetch text from main (change the below code to filter page)
+            if soup.main:
+                return soup.main.get_text()
+            elif soup.article:
+                return soup.article.get_text()
+            else:
+                return soup.body.get_text()
+    except:
+        st.write('something wrong')
+        return None
+
+
+def build_prompt(content, n_chars=300):
+    return f"""以下はとある。Webページのコンテンツである。内容を{n_chars}程度でわかりやすく要約してください。
+
+========
+
+{content[:1000]}
+
+========
+
+日本語で書いてね！
+"""
+
+
+def get_answer(llm, messages):
+    with get_openai_callback() as cb:
+        answer = llm(messages)
+    return answer.content, cb.total_cost
 
 
 def main():
@@ -46,26 +96,39 @@ def main():
     llm = select_model()
     init_messages()
 
-    messages = st.session_state.get('messages', [])
-    for message in messages:
-        if isinstance(message, AIMessage):
-            with st.chat_message('assistant'):
-                st.markdown(message.content)
-        elif isinstance(message, HumanMessage):
-            with st.chat_message('user'):
-                st.markdown(message.content)
-        else:  # isinstance(message, SystemMessage):
-            st.write(f"System message: {message.content}")
+    container = st.container()
+    response_container = st.container()
 
-    # ユーザーの入力を監視
-    user_input = st.chat_input("聞きたいことを入力してね！")
-    if user_input:
-        st.session_state.messages.append(HumanMessage(content=user_input))
-        st.chat_message("user").markdown(user_input)
-        with st.chat_message("assistant"):
-            st_callback = StreamlitCallbackHandler(st.container())
-            response = llm(messages, callbacks=[st_callback])
-        st.session_state.messages.append(AIMessage(content=response.content))
+    with container:
+        url = get_url_input()
+        is_valid_url = validate_url(url)
+        if not is_valid_url:
+            st.write('Please input valid url')
+            answer = None
+        else:
+            content = get_content(url)
+            if content:
+                prompt = build_prompt(content)
+                st.session_state.messages.append(HumanMessage(content=prompt))
+                with st.spinner("ChatGPT is typing ..."):
+                    answer, cost = get_answer(llm, st.session_state.messages)
+                st.session_state.costs.append(cost)
+            else:
+                answer = None
+
+    if answer:
+        with response_container:
+            st.markdown("## Summary")
+            st.write(answer)
+            st.markdown("---")
+            st.markdown("## Original Text")
+            st.write(content)
+
+    costs = st.session_state.get('costs', [])
+    st.sidebar.markdown("## Costs")
+    st.sidebar.markdown(f"**Total cost: ${sum(costs):.5f}**")
+    for cost in costs:
+        st.sidebar.markdown(f"- ${cost:.5f}")
 
 if __name__ == '__main__':
     main()
