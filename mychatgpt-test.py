1dd3bc67155dac08c39a5eefb9189f798ab4aca1
takeumi <39755025+takeyuki1253@users.noreply.github.com>
2023-09-23 18:17:04 +0900

    Update mychatgpt-test.py

--- mychatgpt-test.py
+++ mychatgpt-test.py
@@ -1,128 +1,110 @@
 import streamlit as st
-from streamlit_chat import message
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import (
-    SystemMessage,
-    HumanMessage,
-    AIMessage
-)
 from langchain.callbacks import get_openai_callback
 
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
+from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Qdrant
+
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+
+QDRANT_PATH = "./local_qdrant"
+COLLECTION_NAME = "my_collection"
 
 
 def init_page():
     st.set_page_config(
-        page_title="Website Summarizer",
+        page_title="Ask My PDF(s)",
         page_icon="🤗"
     )
-    st.header("Website Summarizer 🤗")
-    st.sidebar.title("Options")
+    st.sidebar.title("Nav")
+    st.session_state.costs = []
 
 
-def init_messages():
-    clear_button = st.sidebar.button("Clear Conversation", key="clear")
-    if clear_button or "messages" not in st.session_state:
-        st.session_state.messages = [
-            SystemMessage(content="You are a helpful assistant.")
-        ]
-        st.session_state.costs = []
-
-
-def select_model():
-    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
-    if model == "GPT-3.5":
-        model_name = "gpt-3.5-turbo"
+def get_pdf_text():
+    uploaded_file = st.file_uploader(
+        label='Upload your PDF here😇',
+        type='pdf'
+    )
+    if uploaded_file:
+        pdf_reader = PdfReader(uploaded_file)
+        text = '\n\n'.join([page.extract_text() for page in pdf_reader.pages])
+        text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+            model_name=st.session_state.emb_model_name,
+            # 適切な chunk size は質問対象のPDFによって変わるため調整が必要
+            # 大きくしすぎると質問回答時に色々な箇所の情報を参照することができない
+            # 逆に小さすぎると一つのchunkに十分なサイズの文脈が入らない
+            chunk_size=250,
+            chunk_overlap=0,
+        )
+        return text_splitter.split_text(text)
     else:
-        model_name = "gpt-4"
-
-    return ChatOpenAI(temperature=0, model_name=model_name)
-
-
-def get_url_input():
-    url = st.text_input("URL: ", key="input")
-    return url
-
-
-def validate_url(url):
-    try:
-        result = urlparse(url)
-        return all([result.scheme, result.netloc])
-    except ValueError:
-        return False
-
-
-def get_content(url):
-    try:
-        with st.spinner("Fetching Content ..."):
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            # fetch text from main (change the below code to filter page)
-            if soup.main:
-                return soup.main.get_text()
-            elif soup.article:
-                return soup.article.get_text()
-            else:
-                return soup.body.get_text()
-    except:
-        st.write('something wrong')
         return None
 
 
-def build_prompt(content, n_chars=300):
-    return f"""以下はとある。Webページのコンテンツである。内容を{n_chars}程度でわかりやすく要約してください。
+def load_qdrant():
+    client = QdrantClient(path=QDRANT_PATH)
 
-========
+    # すべてのコレクション名を取得
+    collections = client.get_collections().collections
+    collection_names = [collection.name for collection in collections]
 
-{content[:1000]}
+    # コレクションが存在しなければ作成
+    if COLLECTION_NAME not in collection_names:
+        # コレクションが存在しない場合、新しく作成します
+        client.create_collection(
+            collection_name=COLLECTION_NAME,
+            vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
+        )
+        print('collection created')
 
-========
+    return Qdrant(
+        client=client,
+        collection_name=COLLECTION_NAME, 
+        embeddings=OpenAIEmbeddings()
+    )
 
-日本語で書いてね！
-"""
+
+def build_vector_store(pdf_text):
+    qdrant = load_qdrant()
+    qdrant.add_texts(pdf_text)
+
+    # 以下のようにもできる。この場合は毎回ベクトルDBが初期化される
+    # LangChain の Document Loader を利用した場合は `from_documents` にする
+    # Qdrant.from_texts(
+    #     pdf_text,
+    #     OpenAIEmbeddings(),
+    #     path="./local_qdrant",
+    #     collection_name=COLLECTION_NAME,
+    # )
 
 
-def get_answer(llm, messages):
-    with get_openai_callback() as cb:
-        answer = llm(messages)
-    return answer.content, cb.total_cost
+def page_pdf_upload_and_build_vector_db():
+    st.title("PDF Upload")
+    container = st.container()
+    with container:
+        pdf_text = get_pdf_text()
+        if pdf_text:
+            with st.spinner("Loading PDF ..."):
+                build_vector_store(pdf_text)
+
+
+def page_ask_my_pdf():
+    st.title("Ask My PDF(s)")
+    st.write('Under Construction')
+
+    # 後で実装する
 
 
 def main():
     init_page()
 
-    llm = select_model()
-    init_messages()
-
-    container = st.container()
-    response_container = st.container()
-
-    with container:
-        url = get_url_input()
-        is_valid_url = validate_url(url)
-        if not is_valid_url:
-            st.write('Please input valid url')
-            answer = None
-        else:
-            content = get_content(url)
-            if content:
-                prompt = build_prompt(content)
-                st.session_state.messages.append(HumanMessage(content=prompt))
-                with st.spinner("ChatGPT is typing ..."):
-                    answer, cost = get_answer(llm, st.session_state.messages)
-                st.session_state.costs.append(cost)
-            else:
-                answer = None
-
-    if answer:
-        with response_container:
-            st.markdown("## Summary")
-            st.write(answer)
-            st.markdown("---")
-            st.markdown("## Original Text")
-            st.write(content)
+    selection = st.sidebar.radio("Go to", ["PDF Upload", "Ask My PDF(s)"])
+    if selection == "PDF Upload":
+        page_pdf_upload_and_build_vector_db()
+    elif selection == "Ask My PDF(s)":
+        page_ask_my_pdf()
 
     costs = st.session_state.get('costs', [])
     st.sidebar.markdown("## Costs")
